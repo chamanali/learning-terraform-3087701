87c2c543274837fff360840d3cb9eb60654a157d
ChamanAli <chchaman115@outlook.com>
2025-05-19 16:28:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,12 +13,39 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
+data "aws_vpc" "default" {
+default = true
+}
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group.ids = ["aws_security_group.blog.id"]
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = " Allow HTTP/HTTPS inbound. Allow everything out"
+  vpc = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog http in" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+  protocol          = "tcp"
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog http out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
