6266db65831cb576e5f384c79b54e42a369586bb
ChamanAli <chchaman115@outlook.com>
2025-05-19 17:01:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "blog" {
 resource "aws_security_group" "blog" {
   name = "blog"
   description = " Allow HTTP/HTTPS inbound. Allow everything out"
-  vpc = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
